ba4a091865f1a7888d5c57d7df74555e0f985284
Emanuel <emanueleddi2015@gmail.com>
2024-10-04 19:30:58 -0300

    agregue unas variables y una alerta

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+
+let nombreproducto = " Jordan ";
+let preciounitario =  500 ;
+let cantidadeseada = prompt("Cuantas" + nombreproducto + "$" + preciounitario + " desea comprar: " );
+
+let total = preciounitario * cantidadeseada;
+
+alert("el total a pagar por " + cantidadeseada + "" + nombreproducto + "es: $" + total);
+
+
+
+
+
+
+
+
+
